b3ba7fd5f0be5b1c4463809585bf19104dbc9a25
Mark Muthii <mark.muthii@strathmore.edu>
2025-05-21 02:57:08 +0300

    Variables and basic data types

--- 0-hello-world/main.c
+++ 0-hello-world/main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello, World!\n");
+  // Integers
+  int age = 30; // - %i
+  // short, long, long long?
+  
+  // Characters - Single quotes
+  char letter = 'M'; // - %c
+  
+  // strings are represented using double quotations, but as an array of characters
+  // char name = "John Wick"; - Wrong
+  char name[] = "John Wick"; // - Correct - %s
+
+  // A float has less precision than a double, but consumes less memory.
+  float temp = 30.5; // - %f
+  double balance = 100.55; // - %lf
+
+  printf("Hello, %s!\n", name); // Print a string
 }
